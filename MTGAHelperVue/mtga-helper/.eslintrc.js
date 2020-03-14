b8e1db2bb5c7d3345f958d899f3641ba19a2a0ff
dracozombie19 <33298283+dracozombie19@users.noreply.github.com>
2020-03-14 00:08:31 -0500

    Formatting Config Changes

--- MTGAHelperVue/mtga-helper/.eslintrc.js
+++ MTGAHelperVue/mtga-helper/.eslintrc.js
@@ -1,37 +1,30 @@
 module.exports = {
   root: true,
   env: {
-		"browser": true
+    "browser": true
   },
   extends: [
-		"airbnb-base",
-		"plugin:vue/recommended",
-		"prettier",
-		"prettier/vue"
+    "airbnb-base",
+    "plugin:vue/recommended",
+    "prettier",
+    "prettier/vue"
   ],
   plugins: [
-		"prettier",
-		"vue"
-	],
+    "prettier",
+    "vue"
+  ],
   parserOptions: {
-		"parser": "babel-eslint",
-		"sourceType": "module"
+    "parser": "babel-eslint",
+    "sourceType": "module"
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'max-len': ["off"],
     'import/no-unresolved': ["off"],
+    'vue/html-closing-bracket-newline': ["error", {
+      "singleline": "never",
+      "multiline": "never"
+    }]
   },
-  overrides: [
-    {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)',
-      ],
-      env: {
-        mocha: true,
-      },
-    },
-  ],
 };
