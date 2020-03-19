69174a6b4f551b911bac7de7da43b7648533e316
dracozombie19 <33298283+dracozombie19@users.noreply.github.com>
2020-03-18 20:47:00 -0500

    Setting and formatting config
    This configuration still doesn't work like I want, but I'm over it for the time being

--- MTGAHelperVue/mtga-helper/.eslintrc.js
+++ MTGAHelperVue/mtga-helper/.eslintrc.js
@@ -1,30 +1,29 @@
 module.exports = {
-  root: true,
-  env: {
-    "browser": true
-  },
-  extends: [
-    "airbnb-base",
-    "plugin:vue/recommended",
-    "prettier",
-    "prettier/vue"
-  ],
-  plugins: [
-    "prettier",
-    "vue"
-  ],
-  parserOptions: {
-    "parser": "babel-eslint",
-    "sourceType": "module"
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'max-len': ["off"],
-    'import/no-unresolved': ["off"],
-    'vue/html-closing-bracket-newline': ["error", {
-      "singleline": "never",
-      "multiline": "never"
-    }]
-  },
+    root: true,
+    env: {
+        browser: true
+    },
+    extends: ['airbnb-base', 'plugin:vue/recommended', 'prettier', 'prettier/vue'],
+    // extends: ['airbnb-base', 'plugin:prettier/recommended'],
+    // extends: ['plugin:vue/essential',
+
+    // ],
+    plugins: ['prettier', 'vue'],
+    parserOptions: {
+        parser: 'babel-eslint',
+        sourceType: 'module'
+    },
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'max-len': ['off'],
+        'import/no-unresolved': ['off'],
+        'vue/html-closing-bracket-newline': [
+            'error',
+            {
+                singleline: 'never',
+                multiline: 'never'
+            }
+        ]
+    }
 };
