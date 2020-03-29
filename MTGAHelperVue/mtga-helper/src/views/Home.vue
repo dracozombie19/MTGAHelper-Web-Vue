ed9885624cde292362b7d0b520c03b99f1cc3ecc
dracozombie19 <33298283+dracozombie19@users.noreply.github.com>
2020-03-29 16:20:29 -0500

    Home content, PrivacyPolicy formatting

--- MTGAHelperVue/mtga-helper/src/views/Home.vue
+++ MTGAHelperVue/mtga-helper/src/views/Home.vue
@@ -1,18 +1,28 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="content">
+        <h4 class="title is-3"> Welcome to MTGAHelper &mdash; The most complete resource hub for Magic: The Gathering Arena! </h4>
+        <h4 class="title is-4"> Get your Magic Arena decks ideas here. </h4>
+        <p> You will find here all the most important resources and information about Magic: The Gathering Arena, curated and grouped in one place. </p>
+        <p> Here are the most interesting features, among others: </p>
+        <div class="level">
+            <div class="level-item has-text-left">
+                <ul>
+                    <li> Collection tracker: By uploading your collection, you get access to a whole new level of insight. Select your tracked decks and <ul>
+                            <li>Know which packs to prioritize opening in order to complete your tracked decks </li>
+                            <li>Know which cards to craft to progress the most towards the completion of your tracked decks</li>
+                            <li>Identify which decks you are closest to complete</li>
+                        </ul>
+                    </li>
+                    <li>Discover all the Magic Arena decks you can play </li>
+                    <li>Always know about the latest meta </li>
+                </ul>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld,
-  },
+    name: 'Home'
 };
 </script>
